0f869864875b6d405d26769a8e7f77480d6aeb04
truong song <1512823@hcmut.edu.vn>
2021-12-18 10:49:07 +0700

    bt lop doi tuong

---
+++ Lopdoituong/Lopdoituong4/Account.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lopdoituong4
+{
+    class Account
+    {
+        private int Sotaikhoan;
+        private string Tentaikhoan;
+        private double Sodutaikhoan;
+        private double Laisuattaikhoan;
+        public Account(string tentaikhoan,int sotaikhoan,double sodutaikhoan)
+        {
+            this.Tentaikhoan = tentaikhoan;
+            this.Sotaikhoan = sotaikhoan;
+            this.Sodutaikhoan = sodutaikhoan;
+        }
+
+        public double GuiTien(double n)
+        {           
+            if (n<0) 
+            {
+                Console.WriteLine("So tien gui vao khong hop le! vui long kiem tra lai!");
+            }
+            else
+            {
+                Sodutaikhoan = Sodutaikhoan + n;
+            }
+            return Sodutaikhoan;
+        }
+        public double RutTien(double n)
+        {              
+            if (n < 0)
+            {
+                Console.WriteLine("So tien rut khong hop le! vui long kiem tra lai!");
+            }else if (n > Sodutaikhoan)
+            {
+                Console.WriteLine("Giao dich rut tien khong duoc thuc hien!");
+            }
+            else
+            {
+                Sodutaikhoan = Sodutaikhoan - n;
+            }
+            return Sodutaikhoan;
+          
+        }
+        public double Tienlai(double n)
+        {
+            Sodutaikhoan = Sodutaikhoan + 0.045 * n;
+            return Sodutaikhoan;
+        }
+        public double Get_Sotaikhoan()
+        {
+            return Sotaikhoan;
+        }
+        public double Get_Sodu()
+        {           
+            return Sodutaikhoan;
+        }
+        public void print()
+        {
+            Console.WriteLine("name:"+Tentaikhoan);
+            Console.WriteLine("stk:"+Sotaikhoan);
+            Console.WriteLine("sodu:"+Sodutaikhoan);
+        }
+        
+    }
+    class Programing
+    {
+        public static void Main()
+        {
+            Account accbill = new Account("Bill gate", 12345, 100000);                                                  
+            Console.WriteLine("hien thi thong tin:");
+            Console.WriteLine("So tien trong tai khoan cua Bill ban dau la:");
+            accbill.print();
+            Console.WriteLine("So tien trong tai khoan cua Bill khi gui tien vao:");
+            accbill.GuiTien(50000);
+            accbill.Tienlai(50000);
+            accbill.print();
+            Console.WriteLine("So tien trong tai khoan cua Bill sau khi rut tien:");
+            accbill.RutTien(4200);
+            accbill.print();
+            accbill.RutTien(200000);
+        }
+    }
+}
---
+++ Lopdoituong/Lopdoituong5/Mark.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lopdoituong5
+{
+    class Mark
+    {
+        private string Tenmonhoc;
+        private double Diemlt;
+        private double Diemth;
+        public Mark()
+        {
+
+        }
+        public Mark(string tenmonhoc,double diemlt,double diemth)
+        {
+            this.Tenmonhoc = tenmonhoc;
+            this.Diemlt = diemlt;
+            this.Diemlt = diemth;
+        }
+        public void Nhapmonhoc()
+        {
+
+            Console.WriteLine("Nhap ten mon hoc:");
+            Tenmonhoc = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Nhap diem ly thuyet:");
+            Diemlt = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap diem thuc hanh:");
+            Diemth = Convert.ToDouble(Console.ReadLine());
+        }
+        public void Xuatmonhoc()
+        {
+            Console.WriteLine("DANH SANH MON HOC");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Ten mon hoc :" + Tenmonhoc);
+            Console.WriteLine("Diem ly thuyet :" + Diemlt);
+            Console.WriteLine("Diem thuc hanh :" + Diemth);
+            Console.WriteLine("Diem TB:" + tinhDiemtb());
+            Console.WriteLine("\n");
+        }
+        public double tinhDiemtb()
+        {
+            return (Diemlt + Diemth) / 2;
+        }
+    }
+    class Subject
+    {
+        public static void Main()
+        {
+            Console.WriteLine("-------------Menu-------------");
+            Console.WriteLine("|    1-Nhap danh sach mon hoc|");
+            Console.WriteLine("|    2-Hien thi danh sach    |");
+            Console.WriteLine("|    3-Diem TB cao nhat      |");
+            Console.WriteLine("|    4-Thoat                 |");
+            Console.WriteLine("------------------------------");
+            int menu = 0;           
+            ArrayList list = new ArrayList();           
+            int n = 8;
+            do
+            {
+                Console.WriteLine("xin moi ban chon:");
+                menu = Convert.ToInt32(Console.ReadLine());
+                switch (menu)
+                {
+                    case 1:
+                        for (int i = 0; i < 8; i++)
+                        {
+                           
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < 8; i++)
+                        {
+                            ((Mark)list[i]).Xuatmonhoc();
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Mon hoc co die tb cao nhat la:");
+                        Mark max = new Mark();
+                        max = (Mark)list[0];
+                        for (int i = 1; i < 8; i++)
+                        {
+                            if (max.tinhDiemtb() < ((Mark)list[i]).tinhDiemtb())
+                            {
+                                max = (Mark)list[i];
+                            }
+                        }
+                        max.Xuatmonhoc();
+                        break;
+                    case 4:
+                        Console.WriteLine("bye bye ban!chuc ban 1 ngay tot lanh!");
+                        break;
+                    default:
+                        break;
+                }
+            } while (menu!=4);
+        }
+    }
+}
